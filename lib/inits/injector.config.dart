40f6f155346fc54a6d4351277a63799b2b6e4a86
Hussam Elmaghraby <hussamelmaghraby@gmail.com>
2024-12-03 12:50:58 +0200

    updated injector.config.dart

--- lib/inits/injector.config.dart
+++ lib/inits/injector.config.dart
@@ -14,6 +14,8 @@ import 'package:ecommerce_shop/orders/data/repo_impl/orders_repo_impl.dart'
     as _i219;
 import 'package:ecommerce_shop/orders/domain/repositories/orders_repository.dart'
     as _i805;
+import 'package:ecommerce_shop/orders/domain/usecase/get_orders_usecase.dart'
+    as _i985;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
@@ -31,6 +33,8 @@ extension GetItInjectableX on _i174.GetIt {
     gh.factory<_i497.OrdersDataSource>(() => _i497.OrdersDataSource());
     gh.factory<_i805.OrderRespository>(
         () => _i219.OrdersRepoImpl(gh<_i497.OrdersDataSource>()));
+    gh.factory<_i985.GetOrdersUsecase>(() =>
+        _i985.GetOrdersUsecase(orderRespository: gh<_i805.OrderRespository>()));
     return this;
   }
 }
