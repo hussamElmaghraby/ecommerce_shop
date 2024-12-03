b27b299e0de3094443cef3554520fe3bb4d12323
Hussam Elmaghraby <hussamelmaghraby@gmail.com>
2024-12-03 12:47:13 +0200

    injector init.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,12 +1,18 @@
+import 'package:ecommerce_shop/app/core/routing/route_names.dart';
+import 'package:ecommerce_shop/orders/presentation/screens/numeric_metrics_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'core/routing/pages.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return  GetMaterialApp(
+      initialRoute: AppRoutes.METRICS ,
+      getPages: AppPages.routes,
     );
   }
 }
